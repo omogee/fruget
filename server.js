9501be74c84f582ea8d6300c6437f25ce6749fcd
Eze Ogbonnaya <yexies4ogb@gmail.com>
2021-07-25 15:41:58 -0700

    hello

--- server.js
+++ server.js
@@ -248,7 +248,8 @@ app.get('/suggestions/suggestion',(req,res)=>{
     })
   })
 if(process.env.NODE_ENV === "production"){ 
-app.use('/static', express.static(path.join(__dirname, 'ogbmain/build')));
+app.use(express.static("ogbmain/build"))
+
 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname, 'ogbmain','build', 'index.html'));
